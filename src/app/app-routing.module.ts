720f6019f9fa0a3d5727d6d2b7f7e0492e438aa0
ratexbg <83062383+ratexbg@users.noreply.github.com>
2021-12-15 17:37:11 +0200

    dashboard to be added, css fixed for individual players

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayersComponent } from './players/players.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'players', component: PlayerComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
